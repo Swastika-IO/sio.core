7481b9546b26e188f8aca42372e0a746638d0f5a
Smilefounder <i.love.to.smile.around@gmail.com>
2017-06-09 12:16:54 +0100

    Added simple vuejs portal

---
+++ src/Swastika.UI.Site/Controllers/PortalController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Swastika.UI.Base.Extensions;
+using System.Collections.Generic;
+
+namespace Swastika.UI.Site.Controllers
+{
+    [Route("portal")]
+    public class PortalController : Controller
+    {
+        /// <summary>
+        /// Indexes this instance.
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+    }
+}
--- src/Swastika.UI.Site/wwwroot/js/src/main.js
+++ src/Swastika.UI.Site/wwwroot/js/src/main.js
@@ -2,9 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 
 new Vue({
-  el: '#app',
-  //render: h => h(App),
-  data: {
-      title: 'VueJS Hello!'
-  }
+    el: '#app',
+    render: h => h(App)//,
+    //data: {
+    //    title: 'VueJS Hello!'
+    //}
 })
