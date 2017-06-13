d71c72b346171db90815bb33a4d00f7391ed0ba0
Smilefounder <i.love.to.smile.around@gmail.com>
2017-06-13 02:51:37 +0100

    update vue source

--- src/Swastika.UI.Spa/ClientApp/components/portal/portal.ts
+++ src/Swastika.UI.Spa/ClientApp/components/portal/portal.ts
@@ -1,19 +1,11 @@
-//import Vue from 'vue';
-//import { Component } from 'vue-property-decorator';
-
-//@Component({
-//    components: {
-//        MenuComponent: require('../navmenu/navmenu.vue.html')
-//    }
-//})
-//export default class AppComponent extends Vue {
-//}
 import Vue from 'vue';
 import { Component } from 'vue-property-decorator';
 
 @Component({
     components: {
-        AppHeader: require('./app-header/header.vue.html')
+        MenuComponent: require('./navbar/navbar.vue.html'),
+        SidebarComponent: require('./sidebar/sidebar.vue.html'),
+        BreadcrumbComponent: require('./breadcrumb/breadcrumb.vue.html')
     }
 })
 export default class AppComponent extends Vue {
