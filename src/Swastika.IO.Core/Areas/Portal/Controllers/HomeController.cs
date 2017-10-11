13c0b85fc08b786c667c491ad5220917f4fabe36
Smilefounder <huy.nguyen@smileway.org>
2017-10-11 09:27:56 +0000

    init ngx-admin dasboard source

---
+++ src/Swastika.IO.Core/Areas/Portal/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Swastika.IO.Core.Areas.Portal.Controllers
+{
+    [Area("Portal")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            ViewData["RequestId"] = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+            return View();
+        }
+    }
+}
