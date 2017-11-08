e37eddd668d431f5cd1d2238d69c34d4d0183d9d
Hoang Nguyen <nhathoang989@gmail.com>
2017-11-08 18:28:53 +0700

    add helper

---
+++ src/Swastika.Cms.Lib/SWCmsHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Swastika.IO.Cms.Lib
+{
+    public class SWCmsHelper
+    {
+        public static string GetFullPath(string[] subPaths)
+        {
+            string result = string.Empty;
+            string strFormat = string.Empty;
+            for (int i = 0; i < subPaths.Length; i++)
+            {
+                strFormat += @"{" + i + "}" + (i < subPaths.Length - 1 ? "/" : string.Empty);
+            }
+            return string.Format(strFormat, subPaths);
+        }
+    }
+}
