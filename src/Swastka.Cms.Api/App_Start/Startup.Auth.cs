fb498b9005e2d2257d11bf33b8ef45a7f03e8331
Hoang Nguyen <nhathoang989@gmail.com>
2017-11-14 22:37:46 +0700

    Identity server 4

---
+++ src/Swastka.Cms.Api/App_Start/Startup.Auth.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Swastka.IO.Cms.Api.App_Start
+{
+    public partial class Startup
+    {
+    }
+}
---
+++ src/Swastka.Cms.Api/Readme.txt
@@ -0,0 +1,6 @@
+// Identity 
+// Ref: https://elanderson.net/2017/06/identity-server-sample-exploration-and-initial-project-setup/
+
+- Nuget: 
+	+ IdentityServer4 
+	+ IdentityServer4.AccessTokenValidation
