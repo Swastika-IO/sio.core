cca148d163afe2a980b4650bea20f9e11b74f71c
Huy Nguyen <i.love.to.smile.around@gmail.com>
2017-12-09 11:26:53 +0000

    Update list article template (added ng2-smart-table)

--- src/Swastika.IO.Core/ClientApp/app/areas/portal/modules/article/list-article/list-article.component.ts
+++ src/Swastika.IO.Core/ClientApp/app/areas/portal/modules/article/list-article/list-article.component.ts
@@ -7,4 +7,44 @@ import { Component, ViewEncapsulation } from '@angular/core';
     //encapsulation: ViewEncapsulation.None
 })
 export class ProtalListArticleComponent {
+  settings = {
+    columns: {
+      id: {
+        title: 'ID'
+      },
+      name: {
+        title: 'Full Name'
+      },
+      username: {
+        title: 'User Name'
+      },
+      email: {
+        title: 'Email'
+      }
+    }
+  };
+
+  data = [
+    {
+      id: 1,
+      name: "Leanne Graham",
+      username: "Bret",
+      email: "Sincere@april.biz"
+    },
+    {
+      id: 2,
+      name: "Ervin Howell",
+      username: "Antonette",
+      email: "Shanna@melissa.tv"
+    },
+  
+    // ... list of items
+  
+    {
+      id: 11,
+      name: "Nicholas DuBuque",
+      username: "Nicholas.Stanton",
+      email: "Rey.Padberg@rosamond.biz"
+    }
+  ];
 }
--- src/Swastika.IO.Core/ClientApp/app/areas/portal/modules/article/list-article/list-article.module.ts
+++ src/Swastika.IO.Core/ClientApp/app/areas/portal/modules/article/list-article/list-article.module.ts
@@ -1,13 +1,15 @@
-﻿import { NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ProtalListArticleComponent } from './list-article.component';
+import { Ng2SmartTableModule } from 'ng2-smart-table';
 
 @NgModule({
     imports: [
         RouterModule.forChild([{
             path: '',
             component: ProtalListArticleComponent
-        }])
+        }]),
+        Ng2SmartTableModule
     ],
     exports: [RouterModule],
     declarations: [
