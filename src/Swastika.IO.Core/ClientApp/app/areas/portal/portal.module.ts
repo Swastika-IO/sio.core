3051f1c83e2d498d7a376e3a1c32d7a0d376febc
Huy Nguyen <i.love.to.smile.around@gmail.com>
2017-12-09 11:50:35 +0000

    added footer + remove ref default .net projects

--- src/Swastika.IO.Core/ClientApp/app/areas/portal/portal.module.ts
+++ src/Swastika.IO.Core/ClientApp/app/areas/portal/portal.module.ts
@@ -1,44 +1,46 @@
-﻿import { NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { PortalComponent } from './portal.component';
 
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
-    imports: [
-        RouterModule.forChild([
-            {
-                path: '',
-                component: PortalComponent,
-                children: [
-                    //{
-                    //    path: '',
-                    //    redirectTo: 'dashboard',
-                    //    pathMatch: 'full'
-                    //},
-                    {
-                        path: '',
-                        loadChildren: './modules/dashboard/dashboard.module#PortalDashboardModule',
-                    },
-                    {
-                        path: 'article',
-                        loadChildren: './modules/article/article.module#PortalArticleModule',
-                    },
-                    {
-                        path: 'blank',
-                        loadChildren: './modules/_blank/something.module#PortalSomethingModule',
-                    }
-                ]
-            },
-        ])
-    ],
-    exports: [RouterModule],
-    declarations: [
-        PortalComponent,
-        HeaderComponent,
-        SidebarComponent,
-    ]
+  imports: [
+    RouterModule.forChild([
+      {
+        path: '',
+        component: PortalComponent,
+        children: [
+          //{
+          //    path: '',
+          //    redirectTo: 'dashboard',
+          //    pathMatch: 'full'
+          //},
+          {
+            path: '',
+            loadChildren: './modules/dashboard/dashboard.module#PortalDashboardModule',
+          },
+          {
+            path: 'article',
+            loadChildren: './modules/article/article.module#PortalArticleModule',
+          },
+          {
+            path: 'blank',
+            loadChildren: './modules/_blank/something.module#PortalSomethingModule',
+          }
+        ]
+      },
+    ])
+  ],
+  exports: [RouterModule],
+  declarations: [
+    PortalComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent,
+  ]
 })
 
 export class PortalModule { }   
