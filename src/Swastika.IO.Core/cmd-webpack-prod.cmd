a458a2558c15285aa240de4b1bbbd474899585d0
Smilefounder <huy.nguyen@smileway.org>
2017-12-12 11:31:48 +0000

    upd cmd files

---
+++ src/Swastika.IO.Core/cmd-webpack-prod.cmd
@@ -0,0 +1,2 @@
+npm run clean && npm run build:vendor -- --env.prod && npm run build:webpack -- --env.prod
+pause
--- src/Swastika.IO.Core/cmd-webpack.ps1
+++
@@ -1,4 +0,0 @@
-npm i
-node node_modules/webpack/bin/webpack.js --config webpack.config.vendor.js
-node node_modules/webpack/bin/webpack.js
-pause
