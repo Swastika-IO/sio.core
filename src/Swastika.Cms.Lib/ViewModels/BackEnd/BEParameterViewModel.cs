348651c5dc5d160f5f17d34f5c532fdb1febba49
tintintin89 <nhathoang989@hotmail.com>
2018-01-11 09:34:27 +0700

    Add parameter vm

---
+++ src/Swastika.Cms.Lib/ViewModels/BackEnd/BEParameterViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Swastika.Cms.Lib.Models;
+using Swastika.Domain.Data.ViewModels;
+using Microsoft.EntityFrameworkCore.Storage;
+using Newtonsoft.Json;
+using Swastika.IO.Common.Helper;
+using Swastika.IO.Domain.Core.ViewModels;
+using Swastika.Cms.Lib.ViewModels.Info;
+using System.Threading.Tasks;
+using Swastika.Cms.Lib.ViewModels.FrontEnd;
+
+namespace Swastika.Cms.Lib.ViewModels.BackEnd
+{
+    public class BEParameterViewModel
+        : ViewModelBase<SiocCmsContext, SiocParameter, BEParameterViewModel>
+    {
+        #region Properties
+
+        #region Models
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("description")]
+        public string Description { get; set; }
+        [JsonProperty("value")]
+        public string Value { get; set; }
+        #endregion
+
+        #region Views
+
+        #endregion
+
+        #endregion
+
+        #region Contructors
+
+        public BEParameterViewModel() : base()
+        {
+        }
+
+        public BEParameterViewModel(SiocParameter model, SiocCmsContext _context = null, IDbContextTransaction _transaction = null) : base(model, _context, _transaction)
+        {
+        }
+
+        #endregion
+    }
+}
