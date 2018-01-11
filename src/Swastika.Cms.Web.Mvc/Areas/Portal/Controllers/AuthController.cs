459f8e6bbd67ed49c32e608fdd5edfdaa7c30afe
Hoang Nguyen <nhathoang989@gmail.com>
2018-01-11 20:00:01 +0700

    add migrate acc

--- src/Swastika.Cms.Web.Mvc/Areas/Portal/Controllers/AuthController.cs
+++ src/Swastika.Cms.Web.Mvc/Areas/Portal/Controllers/AuthController.cs
@@ -165,7 +165,7 @@ namespace Swastika.Cms.Web.Mvc.Areas.Portal.Controllers
                     //    $"Please confirm your account by clicking this link: <a href='{callbackUrl}'>link</a>");
                     await _signInManager.SignInAsync(user, isPersistent: false);
                     _logger.LogInformation(3, "User created a new account with password.");
-                    return Redirect("/");
+                    return RedirectToAction("Home", "Home", new { culture = _lang });
                 }
                 AddErrors(result);
             }
