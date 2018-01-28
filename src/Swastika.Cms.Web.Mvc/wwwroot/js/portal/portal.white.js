20341b31605e47d6e387cca745a8d0ccaa7e64c9
Huy Nguyen <i.love.to.smile.around@gmail.com>
2018-01-28 10:39:44 +0000

    update priority

--- src/Swastika.Cms.Web.Mvc/wwwroot/js/portal/portal.white.js
+++ src/Swastika.Cms.Web.Mvc/wwwroot/js/portal/portal.white.js
@@ -14,12 +14,14 @@
 
                     //add each li position to the array...     
                     // the +1 is for make it start from 1 instead of 0
-                    order.push($(this).attr('id') + '=' + ($(this).index() + 1));
+                    //order.push($(this).attr('id') + '=' + ($(this).index() + 1));
+                    $(this).find('.mod-priority').val($(this).index() + 1);
+                    //alert($(this).attr('module-priority'));
                 });
                 // join the array as single variable...
                 var positions = order.join(';')
                 //use the variable as you need!
-                alert(positions);
+                //alert(positions);
                 // $.cookie( LI_POSITION , positions , { expires: 10 });
             }
         });
