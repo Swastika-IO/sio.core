bce2d6ffda56b79e29cd6274f3198f867fbb37d2
Huy Nguyen <i.love.to.smile.around@gmail.com>
2018-01-28 10:25:16 +0000

    add sort able module list

--- src/Swastika.Cms.Web.Mvc/wwwroot/js/portal/portal.white.js
+++ src/Swastika.Cms.Web.Mvc/wwwroot/js/portal/portal.white.js
@@ -5,7 +5,23 @@
         });
   
         $(".sortable").sortable({
-          revert: true
+            revert: true,
+            update: function (event, ui) {
+                //create the array that hold the positions...
+                var order = [];
+                //loop trought each li...
+                $('.sortable li').each(function (e) {
+
+                    //add each li position to the array...     
+                    // the +1 is for make it start from 1 instead of 0
+                    order.push($(this).attr('id') + '=' + ($(this).index() + 1));
+                });
+                // join the array as single variable...
+                var positions = order.join(';')
+                //use the variable as you need!
+                alert(positions);
+                // $.cookie( LI_POSITION , positions , { expires: 10 });
+            }
         });
         $(".draggable").draggable({
           cursor: "move",
