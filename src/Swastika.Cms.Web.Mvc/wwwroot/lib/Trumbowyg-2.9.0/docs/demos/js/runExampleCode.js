74f4be436bce6f2f480539e1a0735ed87e3938d1
Smilefounder <i.love.to.smile.around@gmail.com>
2018-01-30 18:34:59 +0000

    upd sidebar + icons

--- src/Swastika.Cms.Web.Mvc/wwwroot/lib/Trumbowyg-2.9.0/docs/demos/js/runExampleCode.js
+++
@@ -1,8 +0,0 @@
-(function($) {
-    'use strict';
-
-    $('.js-code-to-eval').each(function() {
-        eval($(this).text()); // jshint ignore:line
-        $(this).text($(this).text().replace(/'Client-ID\s[a-z0-9]+'/, '\'Client-ID xxxxxxxxxxxx\''));
-    });
-})(jQuery);
