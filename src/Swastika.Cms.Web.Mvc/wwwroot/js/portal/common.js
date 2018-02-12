3ba0197118ebf7ab1e44a4b8521743d88dde2b5a
Smilefounder <i.love.to.smile.around@gmail.com>
2018-02-13 01:36:30 +0700

    fix bug

--- src/Swastika.Cms.Web.Mvc/wwwroot/js/portal/common.js
+++ src/Swastika.Cms.Web.Mvc/wwwroot/js/portal/common.js
@@ -177,7 +177,7 @@
 
             $(".image-preview-modal-lg").on('show.bs.modal', function (event) {
                 var button = $(event.relatedTarget) // Button that triggered the modal
-                var recipient = button.data('data-imgsrc') // Extract info from data-* attributes
+                var recipient = button.data('imgsrc') // Extract info from data-* attributes
                 // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
                 // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
                 var modal = $(this)
