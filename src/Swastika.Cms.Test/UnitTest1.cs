be770b9a06c9ae3396d07769f22b039608a6361b
Smilefounder <i.love.to.smile.around@gmail.com>
2018-03-12 02:13:09 +0700

    upd test sample

--- src/Swastika.Cms.Test/UnitTest1.cs
+++ src/Swastika.Cms.Test/UnitTest1.cs
@@ -1,3 +1,7 @@
+// Licensed to the Swastika I/O Foundation under one or more agreements.
+// The Swastika I/O Foundation licenses this file to you under the GNU General Public License v3.0 license.
+// See the LICENSE file in the project root for more information.
+
 using System;
 using Xunit;
 
@@ -8,7 +12,7 @@ namespace Swastika.Cms.Test
         [Fact]
         public void Test1()
         {
-
+            Assert.Equal(2, 1 + 1);
         }
     }
 }
