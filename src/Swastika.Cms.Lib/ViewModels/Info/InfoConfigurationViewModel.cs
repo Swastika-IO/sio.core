73f9e9a97550df19ef5fcf06440e3070bc9679fb
hoang <nhathoang989@gmail.com>
2018-04-14 07:51:45 +0700

    add InfoConfigurationViewModel

---
+++ src/Swastika.Cms.Lib/ViewModels/Info/InfoConfigurationViewModel.cs
@@ -0,0 +1,56 @@
+﻿// Licensed to the Swastika I/O Foundation under one or more agreements.
+// The Swastika I/O Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.EntityFrameworkCore.Storage;
+using Newtonsoft.Json;
+using Swastika.Cms.Lib.Models.Cms;
+using Swastika.Cms.Lib.Services;
+using Swastika.Common.Helper;
+using Swastika.Domain.Core.ViewModels;
+using Swastika.Domain.Data.ViewModels;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Swastika.Cms.Lib.ViewModels.Info
+{
+    public class InfoConfigurationViewModel
+       : ViewModelBase<SiocCmsContext, SiocConfiguration, InfoConfigurationViewModel>
+    {
+        #region Properties
+
+        #region Models
+
+        [Required]
+        [JsonProperty("keyword")]
+        public string Keyword { get; set; }
+        [JsonProperty("category")]
+        public string Category { get; set; }
+        [JsonProperty("value")]
+        public string Value { get; set; }
+        [JsonProperty("dataType")]
+        public SWCmsConstants.DataType DataType { get; set; }
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        #endregion Models
+
+        #endregion Properties
+
+        #region Contructors
+
+        public InfoConfigurationViewModel()
+            : base()
+        {
+        }
+
+        public InfoConfigurationViewModel(SiocConfiguration model, SiocCmsContext _context = null, IDbContextTransaction _transaction = null)
+            : base(model, _context, _transaction)
+        {
+        }
+
+        #endregion Contructors
+
+        
+    }
+}
