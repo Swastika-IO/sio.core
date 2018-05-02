b85dc3b8b3aa5f7ac1aee585986d164b19bf36cd
Smilefounder <i.love.to.smile.around@gmail.com>
2018-05-02 18:25:11 +0000

    upd page structures and folders

---
+++ src/Swastika.Cms.Web.Mvc/wwwroot/app-portal/pages/article/article-controller.js
@@ -0,0 +1,227 @@
+﻿'use strict';
+app.controller('ProductController', ['$scope', '$rootScope','$routeParams', '$timeout', '$location', 'authService', 'ProductServices',
+    function ($scope, $rootScope, $routeParams, $timeout, $location, authService, articleServices) {
+        $scope.request = {
+            pageSize: '10',
+            pageIndex: 0,
+            status: $rootScope.swStatus[1],
+            orderBy: 'CreatedDateTime',
+            direction: '1',
+            fromDate: null,
+            toDate: null,
+            keyword: ''
+        };
+        
+        $scope.activedArticle = null;
+        $scope.relatedArticles = [];
+        $scope.isBusy = false;
+        $scope.data = [];
+        $scope.errors = [];
+        $scope.message = {
+            class: 'info',
+            content: ''
+        };
+        $scope.lang = $('#lang').val();
+        $scope.range = function (max) {
+            var input = [];
+            for (var i = 1; i <= max; i += 1) input.push(i);
+            return input;
+        };
+
+        $scope.getArticle = function (id) {
+            if (!$scope.isBusy) {
+                $scope.isBusy = true;
+                articleServices.getArticle(id, 'be').then(function (response) {
+                    if (response.isSucceed) {
+                        $scope.activedArticle = response.data;
+                        $rootScope.initEditor();
+                    }
+                    $scope.isBusy = false;
+                    $scope.$apply();
+                }).error(function (a, b, c) {
+                    errors.push(a, b, c);
+                    $scope.isBusy = false;
+                    $("html, body").animate({ "scrollTop": "0px" }, 500);
+                    $scope.$apply();
+                });
+            }
+        };
+
+        $scope.initArticle = function () {
+            if (!$scope.isBusy) {
+                $scope.isBusy = true;
+                articleServices.initArticle('be').then(function (response) {
+                    if (response.isSucceed) {
+                        $scope.activedArticle = response.data;
+                        $rootScope.initEditor();
+                    }
+                    $scope.isBusy = false;
+                    $scope.$apply();
+                }).error(function (a, b, c) {
+                    errors.push(a, b, c);
+                    $scope.isBusy = false;
+                    $("html, body").animate({ "scrollTop": "0px" }, 500);                    
+                });
+            }
+        };
+
+        $scope.loadArticle = function () {
+            if (!$scope.isBusy) {
+                $scope.isBusy = true;
+                var id = $routeParams.id;
+
+                articleServices.getArticle(id,'be').then(function (response) {
+                    if (response.data.isSucceed) {
+                        $scope.activedArticle = response.data.data;
+                        $rootScope.initEditor();
+                    }
+                    else {
+                        alert('failed! ' + data.errors);
+                    }
+
+                }, function (a, b, c) {
+                    errors.push(a, b, c);
+                    $scope.isBusy = false;
+                    $("html, body").animate({ "scrollTop": "0px" }, 500);
+                    
+                });
+            }
+        };
+        $scope.loadArticles = async function (pageIndex) {
+            if (!$scope.isBusy) {
+                if (pageIndex != undefined) {
+                    $scope.request.pageIndex = pageIndex;
+                }
+                if ($scope.request.fromDate != null) {
+                    var d = new Date($scope.request.fromDate);
+                    $scope.request.fromDate = d.toISOString();
+                }
+                if ($scope.request.toDate != null) {
+                    $scope.request.toDate = $scope.request.toDate.toISOString();
+                }
+                var resp = await articleServices.getArticles($scope.request);
+                if (resp.isSucceed) {
+
+                    ($scope.data = resp.data);
+                    $("html, body").animate({ "scrollTop": "0px" }, 500);
+                    $.each($scope.data.items, function (i, article) {
+
+                        $.each($scope.activedArticles, function (i, e) {
+                            if (e.articleId == article.id) {
+                                article.isHidden = true;
+                            }
+                        })
+                    })
+                    setTimeout(function () {
+                        $('[data-toggle="popover"]').popover({
+                            html: true,
+                            content: function () {
+                                var content = $(this).next('.popover-body');
+                                return $(content).html();
+                            },
+                            title: function () {
+                                var title = $(this).attr("data-popover-content");
+                                return $(title).children(".popover-heading").html();
+                            }
+                        });
+                    }, 200);
+                    $("html, body").animate({ "scrollTop": "0px" }, 500);
+                }
+                else {
+                    $scope.errors = resp.errors;
+                    $("html, body").animate({ "scrollTop": "0px" }, 500);
+                }
+                //articleServices.getArticles($scope.request).then(function (result) {
+                //    var resp = result.data;
+                //    if (resp.isSucceed) {
+
+                //        ($scope.data = resp.data);
+                //        $("html, body").animate({ "scrollTop": "0px" }, 500);
+                //        $.each($scope.data.items, function (i, article) {
+
+                //            $.each($scope.activedArticles, function (i, e) {
+                //                if (e.articleId == article.id) {
+                //                    article.isHidden = true;
+                //                }
+                //            })
+                //        })
+                //        $scope.isBusy = false;
+                //        setTimeout(function () {
+                //            $('[data-toggle="popover"]').popover({
+                //                html: true,
+                //                content: function () {
+                //                    var content = $(this).next('.popover-body');
+                //                    return $(content).html();
+                //                },
+                //                title: function () {
+                //                    var title = $(this).attr("data-popover-content");
+                //                    return $(title).children(".popover-heading").html();
+                //                }
+                //            });
+                //        }, 200);
+                //    }
+                //    else {
+                //        alert('failed! ' + data.errors);
+                //    }
+                //}, function (a, b, c) {
+                //    errors.push(a, b, c);
+                //    $scope.isBusy = false;
+                //    $("html, body").animate({ "scrollTop": "0px" }, 500);
+                //    $scope.$apply();
+                //});
+            }
+        };
+
+        $scope.removeArticle = function (id) {
+            if (confirm("Are you sure!")) {
+                articleServices.removeArticle(id).then(function (response) {
+                    $scope.loadArticles();
+                }).error(function (a, b, c) {
+                    errors.push(a, b, c);
+                    $("html, body").animate({ "scrollTop": "0px" }, 500);
+                });;
+            }
+        };
+        $scope.saveArticle = async function (article) {
+            if (!isBusy) {
+
+                article.content = $('.editor-content').val();
+                var resp = await articleServices.saveArticle(article);
+                if (resp.isSucceed) {
+                    $scope.activedArticle = resp.data;
+                    $scope.message.content = 'Thành công';
+                    $scope.message.class = 'success';
+                    $("html, body").animate({ "scrollTop": "0px" }, 500);
+                }
+                else {
+                    $scope.errors = resp.errors;
+                    $("html, body").animate({ "scrollTop": "0px" }, 500);
+                }
+              
+
+                //articleServices.saveArticle(article).then(function (response) {
+                //    if (data.isSucceed) {
+                //        $scope.activedArticle = data.data;
+                //        $scope.message.content = 'Thành công';
+                //        $scope.message.class = 'success';
+                //    }
+                //    else {
+                //        $scope.errors = data.errors;
+                //    }
+                //    $("html, body").animate({ "scrollTop": "0px" }, 500);
+                //    $scope.isBusy = false;
+                //}).error(function (a, b, c) {
+                //    errors.push(a, b, c);
+                //    $scope.isBusy = false;
+                //    $("html, body").animate({ "scrollTop": "0px" }, 500);
+                //});
+            }
+        };
+
+        $scope.$watch('isBusy', function (newValue, oldValue) {
+            if (newValue) {
+                $scope.message.content = '';
+                $scope.errors = [];
+            }
+        });
+    }]);
---
+++ src/Swastika.Cms.Web.Mvc/wwwroot/app-portal/pages/article/article-services.js
@@ -0,0 +1,69 @@
+﻿'use strict';
+app.factory('ArticleServices', ['$http', '$rootScope', 'commonServices', function ($http, $rootScope, commonServices) {
+
+    //var serviceBase = 'http://ngauthenticationapi.azurewebsites.net/';
+
+    var productsServiceFactory = {};
+
+    var settings = commonServices.getSiteSettings();
+
+    var _getArticle = async function (id, type) {
+        var apiUrl = '/api/' + settings.lang + '/article/';
+        var req = {
+            method: 'GET',
+            url: apiUrl + 'details/' + type + '/' + id
+        };
+        var resp = await commonServices.getApiResult(req)
+        return resp.data;
+    };
+
+    var _initArticle = async function (type) {
+        var apiUrl = '/api/' + settings.lang + '/article/';
+        var req = {
+            method: 'GET',
+            url: apiUrl + 'init/' + type,
+        };
+        var resp = await commonServices.getApiResult(req)
+        return resp.data;
+    };
+
+    var _getArticles = async function (request) {
+        var apiUrl = '/api/' + settings.lang + '/article/';
+        var req = {
+            method: 'POST',
+            url: apiUrl + 'list',
+            data: JSON.stringify(request)
+        };
+        var resp = await commonServices.getApiResult(req)
+        return resp.data;
+    };
+
+    var _removeArticle = async function (id) {
+        var apiUrl = '/api/' + settings.lang + '/article/';
+        var req = {
+            method: 'GET',
+            url: apiUrl + 'remove/' + id
+        };
+        var resp = await commonServices.getApiResult(req)
+        return resp.data;
+    };
+
+    var _saveArticle = async function (article) {
+        var apiUrl = '/api/' + settings.lang + '/article/';
+        var req = {
+            method: 'POST',
+            url: apiUrl + 'save',
+            data: JSON.stringify(article)
+        };
+        var resp = await commonServices.getApiResult(req)
+        return resp.data;
+    };
+
+    articlesServiceFactory.getArticle = _getArticle;
+    articlesServiceFactory.initArticle = _initArticle;
+    articlesServiceFactory.getArticles = _getArticles;
+    articlesServiceFactory.removeArticle = _removeArticle;
+    articlesServiceFactory.saveArticle = _saveArticle;
+    return articlesServiceFactory;
+
+}]);
