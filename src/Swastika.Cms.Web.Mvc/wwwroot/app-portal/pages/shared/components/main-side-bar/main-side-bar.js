15662dff353c2b92e221500eb4834a90c9cef1ea
Smilefounder <i.love.to.smile.around@gmail.com>
2018-05-02 18:14:08 +0000

    upd sidebar menu and icons

--- src/Swastika.Cms.Web.Mvc/wwwroot/app-portal/pages/shared/components/main-side-bar/main-side-bar.js
+++ src/Swastika.Cms.Web.Mvc/wwwroot/app-portal/pages/shared/components/main-side-bar/main-side-bar.js
@@ -1,5 +1,4 @@
-﻿
-modules.component('mainSideBar', {
+﻿modules.component('mainSideBar', {
     templateUrl: '/app-portal/pages/shared/components/main-side-bar/main-side-bar.html',
     controller: function ($scope) {
         var ctrl = this;
@@ -8,6 +7,7 @@ modules.component('mainSideBar', {
             {
                 title: 'Articles',
                 shortTitle: 'Articles ...',
+                icon: 'mi mi-ReadingList',
                 subMenus: [
                     {
                         title: 'Create',
@@ -29,19 +29,156 @@ modules.component('mainSideBar', {
             {
                 title: 'Products',
                 shortTitle: 'Products ...',
+                icon: 'mi mi-Package',
                 subMenus: [
                     {
                         title: 'Create',
-                        href: '/backend/product/create',
+                        href: '/admin/product/create',
                         icon: 'mi mi-Add mi-fw'
                     },
                     {
                         title: 'List',
-                        href: '/backend/product/list',
+                        href: '/admin/product/list',
                         icon: 'mi mi-List mi-fw'
                     }
                 ]
             },
+            {
+                title: 'Pages',
+                shortTitle: 'Products ...',
+                icon: 'mi mi-Page',
+                subMenus: [
+                    {
+                        title: 'Create',
+                        href: '/admin/product/create',
+                        icon: 'mi mi-Add mi-fw'
+                    },
+                    {
+                        title: 'List',
+                        href: '/admin/product/list',
+                        icon: 'mi mi-List mi-fw'
+                    }
+                ]
+            },
+            {
+                title: 'Forms',
+                shortTitle: 'Products ...',
+                icon: 'mi mi-wpform',
+                subMenus: [
+                    {
+                        title: 'Create',
+                        href: '/admin/product/create',
+                        icon: 'mi mi-Add mi-fw'
+                    },
+                    {
+                        title: 'List',
+                        href: '/admin/product/list',
+                        icon: 'mi mi-List mi-fw'
+                    }
+                ]
+            },
+            {
+                title: 'Modules',
+                shortTitle: 'Products ...',
+                icon: 'mi mi-ResolutionLegacy',
+                subMenus: [
+                    {
+                        title: 'Create',
+                        href: '/admin/product/create',
+                        icon: 'mi mi-Add mi-fw'
+                    },
+                    {
+                        title: 'List',
+                        href: '/admin/product/list',
+                        icon: 'mi mi-List mi-fw'
+                    }
+                ]
+            },
+            {
+                title: 'Widgets',
+                shortTitle: 'Products ...',
+                icon: 'mi mi-Component',
+                subMenus: [
+                    {
+                        title: 'Create',
+                        href: '/admin/product/create',
+                        icon: 'mi mi-Add mi-fw'
+                    },
+                    {
+                        title: 'List',
+                        href: '/admin/product/list',
+                        icon: 'mi mi-List mi-fw'
+                    }
+                ]
+            },
+            {
+                title: 'Themes',
+                shortTitle: 'Products ...',
+                icon: 'mi mi-Personalize',
+                subMenus: [
+                    {
+                        title: 'Create',
+                        href: '/admin/product/create',
+                        icon: 'mi mi-Add mi-fw'
+                    },
+                    {
+                        title: 'List',
+                        href: '/admin/product/list',
+                        icon: 'mi mi-List mi-fw'
+                    }
+                ]
+            },
+            {
+                title: 'Media',
+                shortTitle: 'Products ...',
+                icon: 'mi mi-Photo2 mi-fw',
+                subMenus: [
+                    {
+                        title: 'Create',
+                        href: '/admin/product/create',
+                        icon: 'mi mi-Add mi-fw'
+                    },
+                    {
+                        title: 'List',
+                        href: '/admin/product/list',
+                        icon: 'mi mi-List mi-fw'
+                    }
+                ]
+            },
+            {
+                title: 'Team',
+                shortTitle: 'Products ...',
+                icon: 'mi mi-Contact',
+                subMenus: [
+                    {
+                        title: 'Create',
+                        href: '/admin/product/create',
+                        icon: 'mi mi-Add mi-fw'
+                    },
+                    {
+                        title: 'List',
+                        href: '/admin/product/list',
+                        icon: 'mi mi-List mi-fw'
+                    }
+                ]
+            },
+            {
+                title: 'Settings',
+                shortTitle: 'Products ...',
+                icon: 'mi mi-Settings mi-spin',
+                subMenus: [
+                    {
+                        title: 'Create',
+                        href: '/admin/product/create',
+                        icon: 'mi mi-Add mi-fw'
+                    },
+                    {
+                        title: 'List',
+                        href: '/admin/product/list',
+                        icon: 'mi mi-List mi-fw'
+                    }
+                ]
+            }
         ]
     },
     bindings: {
