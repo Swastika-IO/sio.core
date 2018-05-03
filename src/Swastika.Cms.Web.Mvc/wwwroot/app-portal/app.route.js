ed13ec99120b1e204381126ba0b6900d6015da1c
hoang <nhathoang989@gmail.com>
2018-05-03 21:04:13 +0700

    add angular portal route

---
+++ src/Swastika.Cms.Web.Mvc/wwwroot/app-portal/app.route.js
@@ -0,0 +1,30 @@
+﻿app.config(function ($routeProvider, $locationProvider, $sceProvider) {
+    $locationProvider.html5Mode(true);
+
+    $routeProvider.when("/backend", {
+        controller: "DashboardController",
+        templateUrl: "/app-portal/pages/dashboard/dashboard.html"
+    });
+
+    $routeProvider.when("/backend/login", {
+        controller: "loginController",
+        templateUrl: "/app-portal/pages/login/login.html"
+    });
+
+    $routeProvider.when("/backend/product/list", {
+        controller: "ProductController",
+        templateUrl: "/app-portal/pages/product/list.html"
+    });
+
+    $routeProvider.when("/backend/product/details/:id", {
+        controller: "ProductController",
+        templateUrl: "/app-portal/pages/product/details.html"
+    });
+
+    $routeProvider.when("/backend/product/create", {
+        controller: "ProductController",
+        templateUrl: "/app-portal/pages/product/details.html"
+    });
+
+    $routeProvider.otherwise({ redirectTo: "/backend" });
+});
