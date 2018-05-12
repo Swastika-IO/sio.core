d401efcb346f272bccbe812e1469ba0da40ac627
hoang <nhathoang989@gmail.com>
2018-05-12 18:25:56 +0700

    Add angular shared components/navigations/navigations.js

---
+++ src/Swastika.Cms.Web.Mvc/wwwroot/app-portal/pages/shared/components/navigations/navigations.js
@@ -0,0 +1,8 @@
+﻿
+modules.component('navs', {
+    templateUrl: '/app-portal/pages/shared/components/navigations/navigations.html',
+    bindings: {
+        prefix: '=',
+        data: '='
+    }
+});
