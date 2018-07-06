fbff60fc66e87209773c6e7cd4b6dadbe3caed04
hoang <nhathoang989@gmail.com>
2018-07-06 12:24:09 +0700

    add Customer

---
+++ src/Swastika.Cms.Lib/Models/Cms/SiocOrder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Swastika.Cms.Lib.Models.Cms
+{
+    public partial class SiocOrder
+    {
+        public SiocOrder()
+        {
+            SiocOrderItem = new HashSet<SiocOrderItem>();
+        }
+
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public string CustomerId { get; set; }
+        public DateTime CreatedDateTime { get; set; }
+        public string CreatedBy { get; set; }
+        public int StoreId { get; set; }
+        public int Status { get; set; }
+
+        public SiocCmsCustomer Customer { get; set; }
+        public SiocCmsUser User { get; set; }
+        public ICollection<SiocOrderItem> SiocOrderItem { get; set; }
+    }
+}
---
+++ src/Swastika.Cms.Lib/Models/Cms/SiocOrderItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Swastika.Cms.Lib.Models.Cms
+{
+    public partial class SiocOrderItem
+    {
+        public int Id { get; set; }
+        public int OrderId { get; set; }
+        public string ProductId { get; set; }
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+        public string PriceUnit { get; set; }
+        public string Specificulture { get; set; }
+        public string Description { get; set; }
+
+        public SiocOrder Order { get; set; }
+        public SiocProduct SiocProduct { get; set; }
+    }
+}
