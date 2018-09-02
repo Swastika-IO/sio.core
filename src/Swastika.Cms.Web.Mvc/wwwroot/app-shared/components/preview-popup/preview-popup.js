f085a3eed494ddc8c006dddc648ba29321c3e61e
hoang <nhathoang989@gmail.com>
2018-09-02 15:27:53 +0700

    opt portal ux

---
+++ src/Swastika.Cms.Web.Mvc/wwwroot/app-shared/components/preview-popup/preview-popup.js
@@ -0,0 +1,12 @@
+﻿modules.component('previewPopup', {
+    templateUrl: '/app-shared/components/preview-popup/preview-popup.html',
+    controller: ['$rootScope', '$scope', function ($rootScope, $scope) {
+        var ctrl = this;
+        ctrl.preview = async function () {
+            
+        };
+    }],
+    bindings: {
+        previewObject: '='
+    }
+});
