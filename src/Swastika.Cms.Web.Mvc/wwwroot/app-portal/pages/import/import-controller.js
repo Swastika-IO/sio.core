ec3d53d85886075a4372a1bbd9de34423e40ca39
hoang <nhathoang989@gmail.com>
2018-09-07 08:17:27 +0700

    add import fe

---
+++ src/Swastika.Cms.Web.Mvc/wwwroot/app-portal/pages/import/import-controller.js
@@ -0,0 +1,53 @@
+﻿'use strict';
+app.controller('ImportFileController',
+    ['$scope', '$rootScope', 'ImportFileServices', 'TranslatorService', 'SiteConfigurationService', 
+        function ($scope, $rootScope, importFileServices, translatorService, siteConfigurationService) {
+
+            $scope.importFile = {
+                title: '',
+                description: '',
+                postedFile: {
+                    file: null,
+                    fullPath: '',
+                    folderName: 'Import',
+                    fileFolder: 'Import',
+                    fileName: '',
+                    extension: '',
+                    content: '',
+                    fileStream: ''
+                }
+            };
+            $scope.importType = 'Language';
+            $scope.types = [
+                'Language',
+                'Configuration'
+            ];
+            $scope.errors = [];
+            $scope.saveImportFile = async function (importFile, importType) {
+                $rootScope.isBusy = true;
+                var resp = await importFileServices.saveImportFile(importFile.postedFile, importType);
+                if (resp && resp.isSucceed) {
+                    $scope.activedImportFile = resp.data;
+                    $rootScope.showMessage('Success', 'success');
+                    switch (importType) {
+                        case 'Language':
+                            translatorService.reset($rootScope.settings.lang).then(function () {
+                                window.location.href = '/backend/language/list';
+                            });
+                            return false;
+                        case 'Configuration':
+                            siteConfigurationService.reset($rootScope.settings.lang).then(function () {
+                                window.location.href = '/backend/configuration/list';
+                            });
+                            return false;
+                    }
+
+
+                }
+                else {
+                    if (resp) { $rootScope.showErrors(resp.errors); }
+                    $rootScope.isBusy = false;
+                    $scope.$apply();
+                }
+            };
+        }]);
---
+++ src/Swastika.Cms.Web.Mvc/wwwroot/app-portal/pages/import/import-services.js
@@ -0,0 +1,23 @@
+﻿'use strict';
+app.factory('ImportFileServices', ['$http', '$rootScope', 'CommonServices', function ($http, $rootScope, commonServices) {
+
+    //var serviceBase = 'http://ngauthenticationapi.azurewebsites.net/';
+
+    var importFilesServiceFactory = {};
+
+    var settings = $rootScope.settings;
+
+    var _saveImportFile = async function (importFile, type) {
+        var apiUrl = '/api/portal/import/' + type;
+        var req = {
+            method: 'POST',
+            url: apiUrl,
+            data: JSON.stringify(importFile)
+        };
+        return await commonServices.getApiResult(req);
+    };
+
+    importFilesServiceFactory.saveImportFile = _saveImportFile;
+    return importFilesServiceFactory;
+
+}]);
