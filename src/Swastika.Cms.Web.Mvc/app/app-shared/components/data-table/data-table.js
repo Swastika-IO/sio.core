919f35ef61b2d6740fd8bec85d99270dcecc1545
hoang <nhathoang989@gmail.com>
2018-09-12 01:26:16 +0700

    add portal data-table

--- src/Swastika.Cms.Web.Mvc/app/app-shared/components/data-table/data-table.js
+++ src/Swastika.Cms.Web.Mvc/app/app-shared/components/data-table/data-table.js
@@ -3,11 +3,20 @@ modules.component('swDataTable', {
     templateUrl: '/app-shared/components/data-table/data-table.html',
     controller: ['$rootScope', '$scope', '$location', function ($rootScope, $scope, $location) {
         var ctrl = this;
+        ctrl.colWidth = 3;
+        ctrl.init = function(){
+            ctrl.colWidth = parseInt(9 / ctrl.columns.length);
+            ctrl.lastColWidth = (9%ctrl.columns.length) > 0 ? 2: 1;                      
+        };
         ctrl.translate = function (keyword) {
             return $rootScope.translate(keyword);
         };
-        ctrl.updatePriority = async function (item) {
-            await service.update(item);
+        ctrl.selected = null;
+        ctrl.updateOrders = function (index) {
+            ctrl.data.items.splice(index, 1);
+            for (var i = 0; i < ctrl.data.items.length; i++) {
+                ctrl.data.items[i].priority = i + 1;
+            }
         };
     }],
     bindings: {
